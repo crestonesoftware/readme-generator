9bf899af66b4d989b19723a27dd774898da1e622
cuhrmunken <elliott.davis@crestonetech.com>
2024-06-21 13:25:54 -0600

    adds default answers to questions

--- assets/js/inquirer-questions.js
+++ assets/js/inquirer-questions.js
@@ -3,16 +3,19 @@ module.exports = [
     {
         type: "input",
         name: "projectTitle",
-        message: "What is the name of your project?"
+        message: "What is the name of your project?",
+        default: "ACME Readme Generator"
     },
     {
         type: "input",
         name: "projectDescription",
-        message: "Describe your project"
+        message: "Describe your project",
+        default: "Generates README.md files from user input, and somehow involves a catapult."
     },
     {
         type: "input",
         name: "installationInstructions",
-        message: "How can users install your product?"
+        message: "How can users install your product?",
+        default: "I haven't worked out the installation instructions yet"
     }
 ];
