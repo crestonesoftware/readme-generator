3dad9da52bc4dd9406d92f8335bb2d51584a0a09
cuhrmunken <elliott.davis@crestonetech.com>
2024-06-21 07:27:11 -0600

    gets description from user

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ const questions = [
         type: "input",
         name: "projectTitle",
         message: "What is the name of your project?"
+    },
+    {
+        type: "input",
+        name: "projectDescription",
+        message: "Describe your project"
     }
 ];
 
@@ -41,18 +46,6 @@ function writeToFile(fileName, data) {
     });
 }
 
-// prompts user to enter information about the project
-// awaits the user's responses before completing execution and returns a promise
-// when calling, be sure to use promptUserForProjectDetails().then() for next steps
-async function promptUserForProjectDetails() {
-    await inquirer.
-        prompt(questions)
-    .then((answers) => {
-        projectTitle = answers.projectTitle;
-        console.log(`in promptUserForProjectDetails(): ${projectTitle}. This one should be first.`);
-    })
-}
-
 // adds a string, followed by a newline, to the contents of the generated readme
 // meant to be called by other functions with specific needs, e.g. to add a Title,
 // create a function that calls this function and pass a string to which you have prepended "# "
@@ -80,14 +73,26 @@ function addSectionToREADME(sectionHeading, sectionContents) {
     addBodyTextToREADME(sectionContents);
 }
 
+// prompts user to enter information about the project
+// awaits the user's responses before completing execution and returns a promise
+// when calling, be sure to use promptUserForProjectDetails().then() for next steps
+async function promptUserForProjectDetails() {
+    await inquirer.
+        prompt(questions)
+    .then((answers) => {
+        projectTitle = answers.projectTitle;
+        console.log(`in promptUserForProjectDetails(): ${answers.projectTitle}. This one should be first.`);
+        addTitleToREADME(answers.projectTitle);
+        addSectionToREADME("Description",`This is the description for lucky project ${answers.projectDescription}`);
+    })
+}
+
 // INIT
 function init() {
     // using .then() directs execution to wait until the promise frompromptUserForProjectDetails()
     // is resolved before continuing
     promptUserForProjectDetails().then(()=> {
         console.log(`in the .then(): ${projectTitle}. This one should be second`);
-        addTitleToREADME(projectTitle);
-        addSectionToREADME("Description",`This is the description for lucky project ${projectTitle}`);
         console.log(readmeContents);
         writeToFile("README.md", readmeContents);
     });
